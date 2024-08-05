8ad1ecb1b865d04e2f927ba2c1a9a2f639fed9e2
Sonu-Sukralia <sonukr023@gmail.com>
2024-08-05 21:04:06 +0530

    secn dagsjk

--- dags/spark-pi.py
+++ dags/spark-pi.py
@@ -3,20 +3,20 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.utils.dates import days_ago
 
-#default_args = {
-#    'owner': 'airflow',
-#    'depends_on_past': False,
-#    'email_on_failure': False,
-#    'email_on_retry': False,
-#    'retries': 1,
-#    'retry_delay': timedelta(minutes=5),
-#}
-
 default_args = {
-    'owner': 'datamasterylab.com',
-    'start_date': datetime(2024, 8, 5),
-    'catchup': True
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
 }
+
+#default_args = {
+#    'owner': 'datamasterylab.com',
+#    'start_date': datetime(2024, 8, 5),
+#    'catchup': True
+#}
 
 dag = DAG(
     'spark_kubernetes_example',
